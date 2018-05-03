421ff185d96d2b90b3fbc12e58d9b326879bf093
Beno Suriano <bjsuriano@gmail.com>
2018-05-03 10:44:50 -0600

    Working two nodes and link.

---
+++ README.md
@@ -0,0 +1,11 @@
+#Plan
+1. Show nodes
+2. Show connection between nodes
+3. Modify text inside of nodes
+
+#Takeaways
+- Documentation was incomplete
+- Be careful... everything is written in TS
+- CSS needs to be imported
+- If nothing displays, check height
+
--- components/index.js
+++ components/index.js
@@ -1,13 +1,15 @@
-import '@tmkelly28/tk-css'
+// import '@tmkelly28/tk-css'
 import './index.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import simpleExampleRender from './SimpleExample'
+
 ReactDOM.render(
   <div id='demo' className='bg-blue column center-xy fill-xy'>
-    <h1>Ready to React</h1>
-    <div>(⌐■_■)</div>
+    <h1>Simple Example</h1>
+    {simpleExampleRender()}
   </div>,
   document.getElementById('app')
 )
