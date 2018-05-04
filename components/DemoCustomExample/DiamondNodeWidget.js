81b18e09055ddfe81500b49eba4cc66eb789a8ff
Horacio Gutierrez <hogutier@yahoo.com>
2018-05-04 11:04:26 -0500

    svg modifications

--- components/DemoCustomExample/DiamondNodeWidget.js
+++ components/DemoCustomExample/DiamondNodeWidget.js
@@ -12,34 +12,22 @@ export class DiamonNodeWidget extends React.Component {
         return (<div className={"diamond-node"} style={{
             position: "relative",
             width: this.props.size,
-            height: this.props.size
+            height: this.props.size/2
         }}>
         <input type="text" style={{"position": "relative", top: 100}} />
-				<svg width={this.props.size} height={this.props.size} dangerouslySetInnerHTML={{
+				<svg width={this.props.size} height={this.props.size/2} dangerouslySetInnerHTML={{
             __html: `
           <g id="Layer_1">
           </g>
           <g id="Layer_2">
-            <polygon fill="yellow" stroke="#000000" stroke-width="3" stroke-miterlimit="10" points="10,` +
-                this.props.size / 2 +
-                ` ` +
-                this.props.size / 2 +
-                `,10 ` +
-                (this.props.size - 10) +
-                `,` +
-                this.props.size / 2 +
-                ` ` +
-                this.props.size / 2 +
-                `,` +
-                (this.props.size - 10) +
-                `"/>
+          <rect fill="grey" x="0" y="0" rx="10" ry="10" width="${this.props.size}" height="${this.props.size/2}"/>
           </g>
         `
         }}/>
-				<div style={{
+				<div id="test" style={{
             position: "absolute",
             zIndex: 10,
-            top: this.props.size / 2 - 8,
+            top: this.props.size / 4 + 8,
             left: -8
         }}>
 					<PortWidget name="left" node={this.props.node}/>
@@ -48,7 +36,7 @@ export class DiamonNodeWidget extends React.Component {
             position: "absolute",
             zIndex: 10,
             left: this.props.size / 2 - 8,
-            top: -8
+            top: 10
         }}>
 					<PortWidget name="top" node={this.props.node}/>
 				</div>
